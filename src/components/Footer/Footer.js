ecd72c1f49b2c30fb53df05e4285c272170a11a6
Hidayat <hidayetmmdff@gmail.com>
2021-03-10 14:26:39 +0400

    footer fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,29 +9,35 @@ import {
   Typography,
   Container,
 } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 import useStyles from "./styles";
 
 const Footer = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
+    <>
       <AppBar position="static" className={classes.footer} color="inherit">
         <Container
         //  maxWidth="md"
         >
           <Toolbar>
-            <Typography
-              variant="body1"
-              color="inherit"
-              className={classes.typography}
-            >
+            <Typography variant="body1" color="inherit" component="div">
               Gent's Shop | The Sharpest Shop
+            </Typography>
+            <div className={classes.grow} />
+            <Typography variant="body1" color="inherit" component="div">
+              Follow us:
+              <IconButton children={<FacebookIcon />} />
+              <IconButton children={<InstagramIcon />} />
+              <IconButton children={<WhatsAppIcon />} />
             </Typography>
           </Toolbar>
         </Container>
       </AppBar>
-    </div>
+    </>
   );
 };
 
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,19 +3,15 @@ import { makeStyles, fade } from "@material-ui/core/styles";
 const drawerWidth = 0;
 
 export default makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
   footer: {
     boxShadow: "none",
-    borderBottom: "1px solid rgba(0, 0, 0, 0.12)",
+    borderTop: "1px solid rgba(0, 0, 0, 0.12)",
     [theme.breakpoints.up("sm")]: {
       width: `calc(100% - ${drawerWidth}px)`,
       marginLeft: drawerWidth,
     },
   },
-  typography: {
+  grow: {
     flexGrow: 1,
-    // align: "center",
   },
 }));
