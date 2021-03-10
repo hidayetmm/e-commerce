1021bfd8d5cffe856532673f1657715cf92d5e13
Hidayat <hidayetmmdff@gmail.com>
2021-03-10 17:03:26 +0400

    UI fixes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,15 +3,17 @@ import {
   AppBar,
   Toolbar,
   IconButton,
-  Badge,
-  MenuItem,
-  Menu,
+  Link,
   Typography,
   Container,
+  Grid,
 } from "@material-ui/core";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import CallIcon from "@material-ui/icons/Call";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
+
 import useStyles from "./styles";
 
 const Footer = () => {
@@ -20,22 +22,41 @@ const Footer = () => {
   return (
     <>
       <AppBar position="static" className={classes.footer} color="inherit">
-        <Container
-        //  maxWidth="md"
-        >
-          <Toolbar>
-            <Typography variant="body1" color="inherit" component="div">
-              Gent's Shop | The Sharpest Shop
-            </Typography>
-            <div className={classes.grow} />
-            <Typography variant="body1" color="inherit" component="div">
-              Follow us:
-              <IconButton children={<FacebookIcon />} />
-              <IconButton children={<InstagramIcon />} />
-              <IconButton children={<WhatsAppIcon />} />
-            </Typography>
-          </Toolbar>
-        </Container>
+        <Toolbar>
+          <Grid
+            container
+            // direction="row"
+            // justify="center"
+            alignItems="center"
+          >
+            <Grid item md={6} sm={6} xs={12}>
+              <Typography variant="body1" color="inherit" component="div">
+                Gent's Shop | The Sharpest Shop
+              </Typography>
+            </Grid>
+            <Grid item md={6} sm={6} xs={12}>
+              <Typography
+                variant="body1"
+                color="inherit"
+                style={{ display: "inline-block" }}
+              >
+                Contact:
+              </Typography>
+              <IconButton component={Link} to="/cart" aria-label="Facebook">
+                <FacebookIcon color="primary" />
+              </IconButton>
+              <IconButton component={Link} to="/cart" aria-label="Instagram">
+                <InstagramIcon color="secondary" />
+              </IconButton>
+              <IconButton component={Link} to="/cart" aria-label="Whatsapp">
+                <WhatsAppIcon color="action" />
+              </IconButton>
+              <IconButton component={Link} to="/cart" aria-label="Call">
+                <CallIcon color="action" />
+              </IconButton>
+            </Grid>
+          </Grid>
+        </Toolbar>
       </AppBar>
     </>
   );
