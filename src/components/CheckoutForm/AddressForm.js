86f90e633a38c874225cb2f0701fae9a1ec6f000
Hidayat <hidayetmmdff@gmail.com>
2021-03-13 22:30:58 +0400

    Card design fixes

--- src/components/CheckoutForm/AddressForm.js
+++ src/components/CheckoutForm/AddressForm.js
@@ -1,5 +1,24 @@
+import React from "react";
+import {
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Grid,
+  Typhography,
+  Typography,
+} from "@material-ui/core";
+import { useForm, FormProvider } from "react-hook-form";
+
 const AddressForm = () => {
-  return <div>AddressForm</div>;
+  const methods = useForm();
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        Shipping Address
+      </Typography>
+    </>
+  );
 };
 
 export default AddressForm;
